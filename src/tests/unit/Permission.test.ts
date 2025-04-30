379df8a2c508843e5a7dd1c9440e30d03fdd147f
Luis del Amo <luisdelamo@gmail.com>
2025-04-30 15:26:20 +0200

    feat: has role user

--- src/tests/unit/Permission.test.ts
+++ src/tests/unit/Permission.test.ts
@@ -1,28 +1,39 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { UserPermission, UserRole } from '../../infrastructure/permissions';
 
 class Permission {
+  private static roles: UserRole[] = [];
   constructor(readonly name: string) {}
 
-  static create(name: string): Permission {
-    this.validate();
+  static create(name: string, permission: UserPermission): Permission {
+    this.validate(permission);
     return new Permission(name);
   }
 
-  private static validate(): void {
-    throw new Error('should contain at least one role');
+  private static validate(permission: UserPermission): void {
+    this.roles = [...permission.role];
+    if (this.roles.length === 0) {
+      throw new Error('should contain at least one role');
+    }
+  }
+
+  hasRole(role: UserRole): boolean {
+    return Permission.roles.includes(role);
   }
 }
 
 describe('Permission test', () => {
-  it('happy path permissions test', () => {
-    const permission = Permission.create(' permission test');
-
-    expect(permission).toBeInstanceOf(Permission);
-  });
-
   it('should throw an error with message: ', () => {
     expect(() => {
-      Permission.create(' permission test');
+      Permission.create(' permission test', { role: [] } as UserPermission);
     }).toThrowError('should contain at least one role');
+  });
+
+  it('Add Roles to permission and should has permission for that role', () => {
+    const permission = Permission.create(' permission test', {
+      role: [UserRole.USER],
+    } as UserPermission);
+
+    expect(permission.hasRole(UserRole.USER)).toBe(true);
   });
 });
