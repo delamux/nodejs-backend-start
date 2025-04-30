c54abdb63e34fbd14426127b0ad19069f15569fa
Luis del Amo <luisdelamo@gmail.com>
2025-04-30 16:08:13 +0200

    refactor: move permission class

--- src/tests/unit/Permission.test.ts
+++ src/tests/unit/Permission.test.ts
@@ -1,55 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { UserPermission, UserRole } from '../../infrastructure/permissions';
 import { Routes } from '../../infrastructure/routes';
-
-class Permission {
-  private static roles: UserRole[] = [];
-  private static path: Routes;
-
-  constructor(readonly name: string) {}
-
-  static create(name: string, permission: UserPermission): Permission {
-    this.validate(permission);
-    return new Permission(name);
-  }
-
-  private static validate(permission: UserPermission): void {
-    this.validateRole(permission.role);
-    this.validatePath(permission.path);
-  }
-
-  private static validateRole(roles: UserRole[]): void {
-    if (roles.length === 0) {
-      throw new Error('Should contain at least one role');
-    }
-
-    if (!roles.some(role => Object.values(UserRole).includes(role))) {
-      throw new Error('Should has a valid role');
-    }
-
-    this.roles = [...roles];
-  }
-
-  private static validatePath(path: Routes): void {
-    if (!path) {
-      throw new Error('Should contain at least one path allowed');
-    }
-
-    if (!Object.values(Routes).includes(path)) {
-      throw new Error('Should have a valid path');
-    }
-
-    this.path = path;
-  }
-
-  hasRole(role: UserRole): boolean {
-    return Permission.roles.includes(role);
-  }
-
-  hasPath(path: Routes): boolean {
-    return Permission.path === path;
-  }
-}
+import { Permission } from '../../infrastructure/Permission';
 
 describe('Permission test', () => {
   it('should throw an error when is not Role assigned ', () => {
